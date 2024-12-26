4d2f6cc219f366da6dacc4da99aba0eaf0886e77
AbdulRashik <abdulrasik78@gmail.com>
2024-12-26 22:55:34 +0530

    addition

--- src/App.js
+++ src/App.js
@@ -1,14 +1,28 @@
 import "./App.css";
+import React , {useState} from 'react';
 import Router from "./routes";
 import "bootstrap/dist/css/bootstrap.min.css";
 import { Provider } from "react-redux";
 import store from "./Redux/store";
+import { UserLogin } from "./components/UserLogin";
 
 function App() {
+  const [isLoggedIn,setIsLoggedIn] = useState(false);
+
+  const handleLogin = (username,password)=>{
+    if (username === "rashik78@gmail.com" && password === "Zxcv12@bn") {
+      setIsLoggedIn(true);
+    } else {
+      alert("Invalid credentials");
+    }
+    }
+  
   return (
     <Provider store={store}>
       <div className="App">
-        <Router />
+        {isLoggedIn ? (<Router />)
+        :  (<UserLogin onLogin = {handleLogin} />)
+      }
       </div>
     </Provider>
   );
